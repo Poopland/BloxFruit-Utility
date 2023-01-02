00c4c5e507c23a23812f6c023fc7e6ef5a70bdf1
Mexuaita <66193126+Poopland@users.noreply.github.com>
2023-01-02 23:26:11 +0700

    Create Materials.lua

---
+++ Materials.lua
@@ -0,0 +1,23 @@
+local Materials = {
+    ["Magma Ore"] = {"Military Soldier","Military Spy","Magma Admiral","Magma Ninja","Lava Pirate"},
+    ["Leather"] = {"Pirate","Brute","Swan Pirate","Jungle Pirates"},
+    ["Angel Wings"] = {"God's Guard","Shanda","Royal Squad","Royal Soldier","Wysper","Thunder God"},
+    ["Fish Tails"] = {"Fishman Warrior","Fishman Commando","Fishman Lord","Fishman Raider","Fishman Captain"},
+    ["Bones"] = {"Reborn Skeleton","Living Zombie","Demonic Soul","Possessed Mummy"}
+    ["Ectoplasm"] = {"The Cursed Captain","Ship Deckhand","Ship Engineer","Ship Steward","Ship Officer"},
+    ["Radioactive Material"] = {"Factory Staff","Core"},
+    ["Mystic Droplets"] = {"Sea Soldier","Water Fighter","Tide Keeper"},
+    ["Dragon Scales"] = {"Dragon Crew Warrior","Dragon Crew Archer"},
+    ["Conjured Cocoa"] = {"Cocoa Warrior","Chocolate Bar Battler"},
+    ["Dark Fragments"] = {"Darkbeard"},
+    ["Meteorite"] = {"Fajita"},
+    ["Demonic Wisps"] = {"Demonic Soul"},
+    ["Mini Tusk"] = {"Mythological Pirates"},
+    ["Gunpowder"] = {"Pistol Billionaire"},
+    ["Mirror Fractal"] = {"Dough King"},
+    ["Vampire Fangs"] = {"Vampire"},
+    ["Yeti's Fur"] = {"Yeti"},
+    
+}
+Materials["Scrap Metal"] = Materials.Leather
+return Materials
